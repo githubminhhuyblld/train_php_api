3423b483301a569bc293d9f71cb9b56d1e3b624c
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2023-11-19 12:11:27 +0700

    update

--- query/selectCouponsByUserIdProductIdStoreId.sql
+++ query/selectCouponsByUserIdProductIdStoreId.sql
@@ -1,17 +1,24 @@
-SELECT c.* 
+SELECT c.*
 FROM coupon c
 LEFT JOIN user_coupon uc ON c.id = uc.coupon_id
 LEFT JOIN product_coupon pc ON c.id = pc.coupon_id
 LEFT JOIN store_coupon sc ON c.id = sc.coupon_id
 WHERE 
+    (
+        (c.start_date IS NOT NULL AND c.start_date <= CURDATE() AND (c.end_date IS NULL OR c.end_date > CURDATE()))
+        OR
+        (c.start_date IS NULL AND c.end_date IS NOT NULL AND c.end_date > CURDATE())
+    )
+AND 
     (IFNULL(:userId, 0) = 0 OR uc.user_id = :userId)
 AND 
     (IFNULL(:productId, 0) = 0 OR pc.product_id = :productId)
 AND 
     (IFNULL(:storeId, 0) = 0 OR sc.store_id = :storeId)
-AND
-    (CURDATE() BETWEEN c.start_date AND c.end_date)
 GROUP BY c.id;
+
+
+
 
 -- SELECT c.* 
 -- FROM coupon c
